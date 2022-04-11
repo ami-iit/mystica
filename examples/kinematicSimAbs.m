413278a5db38f9be0ff287bb440290500ce997c9
Fabio Bergonti <fabio.bergonti@iit.it>
2022-04-11 14:22:55 +0200

    examples

---
+++ examples/kinematicSimAbs.m
@@ -0,0 +1,33 @@
+%% kinematic Simulation controlling mBodyTwist_0
+
+clear
+clc
+close all
+fclose('all');
+
+% generate Model
+
+switch '4barLinkage'
+    case 'cylinder_triangleLinks'
+        model = mystica.model.getModelCoverTriangleLinks('n',3,'m',5,'restConfiguration','cylinder','linkDimension',0.0425);
+    case 'flat_triangleLinks'
+        model = mystica.model.getModelCoverTriangleLinks('n',3,'m',5,'restConfiguration','flat','linkDimension',0.0425);
+    case 'flat_squareLinks'
+        model = mystica.model.getModelCoverSquareLinks('n',3,'m',5,'restConfiguration','flat','linkDimension',0.0425);
+    case '4barLinkage'
+        model = mystica.model.getModel4BarLinkage();
+end
+
+% define simulation settings and parameters
+
+stgs  = mystica.stgs.getDefaultSettingsSimKinAbs(model);
+
+% run simulation
+
+data  = mystica.runSimKinAbs('model',model,'stgs',stgs,'mBodyPosQuat_0',model.getMBodyPosQuatRestConfiguration,'nameControllerClass','mystica.controller.ExampleKinAbs');
+
+% visualize data
+
+if stgs.visualizer.run
+    mystica.viz.visualizeKinAbs('model',model,'data',data,'stgs',stgs);
+end
---
+++ examples/kinematicSimRel.m
@@ -0,0 +1,37 @@
+%% kinematic Simulation controlling motorsAngVel
+
+clear
+clc
+close all
+fclose('all');
+
+% generate Model
+
+switch 'flat_squareLinks'
+    case 'flat_squareLinks'
+        model = mystica.model.getModelCoverSquareLinks('n',6,'m',3,'restConfiguration','cylinder','linkDimension',0.0425);
+        % assign motors location
+        booleanActuatedJointAngVel = zeros(model.constants.jointsAngVel,1); 
+        booleanActuatedJointAngVel([3,6,9,15,16,21,24,27,30,33,36,45,48,51,53,60,66,69,75,78,81]) = 1;
+        booleanActuatedJointAngVel = reshape(booleanActuatedJointAngVel,model.constants.linkEul,[]);
+        for j = 1:model.nJoint
+            model.actuateJoint('jointIndex',j,'axesActuated',booleanActuatedJointAngVel(:,j),'byPassWarning',1);
+        end
+        clear booleanActuatedJointAngVel
+    case '4barLinkage'
+        model = mystica.model.getModel4BarLinkage();
+end
+
+% define simulation settings and parameters
+
+stgs = mystica.stgs.getDefaultSettingsSimKinRel(model);
+
+% run simulation
+
+data = mystica.runSimKinRel('model',model,'stgs',stgs,'mBodyPosQuat_0',model.getMBodyPosQuatRestConfiguration,'nameControllerClass','mystica.controller.ExampleKinRel');
+
+% visualize data
+
+if stgs.visualizer.run
+    mystica.viz.visualizeKinRel('model',model,'data',data,'stgs',stgs)
+end
