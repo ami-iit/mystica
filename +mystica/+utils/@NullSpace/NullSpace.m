0b9e7dce10bc781e923064d36c8a2dc2708b277b
Fabio Bergonti <fabio.bergonti@gmail.com>
2022-08-03 10:20:04 +0200

    implement pinv

--- +mystica/+utils/@NullSpace/NullSpace.m
+++ +mystica/+utils/@NullSpace/NullSpace.m
@@ -1,8 +1,9 @@
 classdef NullSpace < handle
-    
+
     properties (SetAccess = protected, GetAccess = public)
         A
         Z
+        pinvAred
         rank = []
         singValues
         stgs
@@ -12,7 +13,7 @@ classdef NullSpace < handle
         linIndRow
         V
     end
-    
+
     methods
         function obj = NullSpace(input)
             arguments
@@ -24,12 +25,12 @@ classdef NullSpace < handle
             obj.stgs.decompositionMethod   = input.decompositionMethod;
             obj.stgs.rankRevealingMethod   = input.rankRevealingMethod;
             obj.stgs.toleranceRankRevealing = input.toleranceRankRevealing;
-            
+
             if isempty(input.A) == 0
                 obj.compute(input.A)
             end
         end
-        
+
         function Z = compute(obj,A)
             switch obj.stgs.decompositionMethod
                 case 'qrFull'
@@ -44,7 +45,7 @@ classdef NullSpace < handle
             end
             Z = obj.Z;
         end
-        
+
         function linIndRow = getLinIndRow(obj)
             switch obj.stgs.decompositionMethod
                 case 'svd'
@@ -53,28 +54,39 @@ classdef NullSpace < handle
             end
             linIndRow = obj.linIndRow;
         end
-        
+
     end
-    
+
     methods (Access=protected)
         getRank(obj)
-        
+
         function qr(obj)
-            [Q,R,p] = qr(transpose(obj.A),'vector');
+            [Q,R,P] = qr(transpose(obj.A));
+            [p,~] = find(P); p=p';
             obj.singValues = full(abs(diag(R)));
             obj.V = full(Q);
             obj.getRank();
             obj.Z = obj.V(:,(obj.rank+1):end);
             obj.linIndRow = sort(p(1:obj.rank));
+            %
+            Q1 = Q(:,1:obj.rank);
+            R1 = R(1:obj.rank,1:obj.rank);
+            P1 = P(:,1:obj.rank); P1(sum(P1,2)==0,:)=[];
+            obj.pinvAred = Q1*((R1')\(inv(P1)));
         end
-        
+
         function svd(obj)
-            [~,S,obj.V] = svd(obj.A,0);
+            [U,S,obj.V] = svd(obj.A,0);
             obj.singValues = diag(S);
             obj.getRank();
             obj.Z = obj.V(:,(obj.rank+1):end);
+            %
+            Ur = U(:,1:obj.rank);
+            Sr = S(1:obj.rank,1:obj.rank); invSr = diag(1./diag(Sr));
+            Vr = obj.V(:,1:obj.rank);
+            obj.pinvAred = Vr * invSr * Ur';
         end
-        
+
     end
-    
+
 end
